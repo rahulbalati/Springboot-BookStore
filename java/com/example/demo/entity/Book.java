122d72a2585e2b41996bb2fde55d7f9ca2eb1377
Rahul balati <118408469+rahulbalati@users.noreply.github.com>
2024-03-19 01:18:52 +0530

    Add files via upload
    The Spring Boot book store management system is a web application that allows users to register new books, view available books, and manage their book inventory. It utilizes Spring Boot The system provides a user-friendly interface for efficient book management and tracking.

---
+++ java/com/example/demo/entity/Book.java
@@ -0,0 +1,61 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String author;
+	private String price;
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Book(int id, String author, String price,String name) {
+		super();
+		this.id = id;
+		this.author = author;
+		this.price = price;
+		this.name=name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+
+}
